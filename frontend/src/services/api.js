3b3eeb1945f37bf5e87abcca7a96b468b98ef114
Prince Yadav <167596785+warrior-hub@users.noreply.github.com>
2026-09-26 04:34:40 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://connecto-z1wn.onrender.com//api",
   withCredentials: true,
 });
 
